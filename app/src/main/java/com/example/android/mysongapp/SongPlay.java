761e64ed2ed6971a652cbe2f92380e4827b5158a
Johnkary <john.stef.kary@gmail.com>
2017-11-03 21:11:04 +0200

    Changed MediaPlayer to ExoPlayer --TODO Check if exoplayer is playing, fix logic for stop --TODO faster loading(check two requests)

--- app/src/main/java/com/example/android/mysongapp/SongPlay.java
+++ app/src/main/java/com/example/android/mysongapp/SongPlay.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.extractor.ExtractorSampleSource;
+import com.google.android.exoplayer.upstream.Allocator;
+import com.google.android.exoplayer.upstream.DataSource;
+import com.google.android.exoplayer.upstream.DefaultAllocator;
+import com.google.android.exoplayer.upstream.DefaultUriDataSource;
+import com.google.android.exoplayer.util.Util;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,8 +37,11 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import wseemann.media.FFmpegMediaPlayer;
 
 public class SongPlay extends AppCompatActivity {
     private TextView artist_Text_View;
@@ -39,8 +53,19 @@ public class SongPlay extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private Context context;
 
+    private ExoPlayer exoPlayer;
+    private static final int BUFFER_SEGMENT_SIZE = 64 * 1024;
+    private static final int BUFFER_SEGMENT_COUNT = 256;
+
+
+
+
     private Intent myAdapterIntent;
     private String URL = ServerURL.getURL() + "/play/";
+    private int mainHandler;
+
+
+
 
 
 
@@ -69,6 +94,7 @@ public class SongPlay extends AppCompatActivity {
         String songName = myAdapterIntent.getStringExtra("songName");
 
 
+
         artist_Text_View.setText("Artist :" + " " + artistName);
         album_Text_View.setText("Album :" + "" + albumName);
         song_Text_View.setText("Song  :" + " " + songName);
@@ -78,6 +104,10 @@ public class SongPlay extends AppCompatActivity {
 
         playButton.setOnClickListener(handlePlayButton());
         stopButton.setOnClickListener(handleStopButton());
+
+
+
+
 
 
 
@@ -100,35 +130,85 @@ public class SongPlay extends AppCompatActivity {
     }
 
     private View.OnClickListener handlePlayButton() {
+
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                
+
+//                if(mediaPlayer == null){
+//                    String songPath = myAdapterIntent.getStringExtra("songURL");
+//
+//                    String URLtoPlay = URL + songPath.replaceAll(" ", "%20");
+//
+//                    audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+//
+//                    mediaPlayer = new MediaPlayer();
+//
+//                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//                    try {
+//                        mediaPlayer.reset();
+//                        mediaPlayer.setDataSource(URLtoPlay);
+//                        mediaPlayer.prepare();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    mediaPlayer.start();
+//
+//
+//                }
+//                else{
+//                    return;
+//                }
+                String songPath = myAdapterIntent.getStringExtra("songURL");
+                String URLtoPlay = URL + songPath.replaceAll(" ", "%20");
+                Uri Urlfor = Uri.parse(URLtoPlay);
+//
+//                FFmpegMediaPlayer mp = new FFmpegMediaPlayer();
+//                mp.setOnPreparedListener(new FFmpegMediaPlayer.OnPreparedListener() {
+//
+//                    @Override
+//                    public void onPrepared(FFmpegMediaPlayer mp) {
+//                        mp.start();
+//                    }
+//                });
+//                mp.setOnErrorListener(new FFmpegMediaPlayer.OnErrorListener() {
+//
+//                    @Override
+//                    public boolean onError(FFmpegMediaPlayer mp, int what, int extra) {
+//                        mp.release();
+//                        return false;
+//                    }
+//                });
+//
+//                try {
+//                    mp.setDataSource(URLtoPlay);
+//                    mp.prepareAsync();
+//                } catch (IllegalArgumentException e) {
+//                    e.printStackTrace();
+//                } catch (SecurityException e) {
+//                    e.printStackTrace();
+//                } catch (IllegalStateException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                exoPlayer = ExoPlayer.Factory.newInstance(1);
 
 
-                if(mediaPlayer == null){
-                    String songPath = myAdapterIntent.getStringExtra("songURL");
 
-                    String URLtoPlay = URL + songPath.replaceAll(" ", "%20");
-
-                    audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-
-                    mediaPlayer = new MediaPlayer();
-
-                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    try {
-                        mediaPlayer.reset();
-                        mediaPlayer.setDataSource(URLtoPlay);
-                        mediaPlayer.prepare();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    mediaPlayer.start();
+// Settings for exoPlayer
+                Allocator allocator = new DefaultAllocator(BUFFER_SEGMENT_SIZE);
+                String userAgent = Util.getUserAgent(context, "SongPlay");
+                DataSource dataSource = new DefaultUriDataSource(context, null, userAgent);
+                ExtractorSampleSource sampleSource = new ExtractorSampleSource(
+                        Urlfor, dataSource, allocator, BUFFER_SEGMENT_SIZE * BUFFER_SEGMENT_COUNT);
+                MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource);
+// Prepare ExoPlayer
+                exoPlayer.prepare(audioRenderer);
+                exoPlayer.setPlayWhenReady(true);
 
 
-                }
-                else{
-                    return;
-                }
             }
         };
     }
